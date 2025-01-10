77452815b53bd8876692ac8bd69ec557360aa552
raushan <rk0383944@gmail.com>
2025-01-10 12:58:56 +0530

    server path changed

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:4000/api/v1'
-// export const BASE_URL = 'https://tour-full-stack-rosy.vercel.app/api/v1'
-// export const BASE_URL = 'https://tour-full-stack-rosy.vercel.app';
+// export const BASE_URL = 'http://localhost:4000/api/v1'
+export const BASE_URL = 'https://tours-server.vercel.app/api/v1'
+
