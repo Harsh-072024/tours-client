38b120b26c2a3eaa6da017c6defb08871574d24f
Raushan Kumar <rk0383944@gmail.com>
2025-01-12 09:24:01 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = 'http://localhost:4000/api/v1'
-export const BASE_URL = 'https://tours-server.vercel.app/api/v1'
+export const BASE_URL = 'https://tours-server-production.up.railway.app/api/v1'
 
